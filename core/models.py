b38ab48c05195ba35c6bc7c78c373517b40cac6b
barto <bartosz.marmolowski@gmail.com>
2020-10-04 11:32:27 +0200

    Add Foreign Key to Product model

--- core/models.py
+++ core/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 
-class Product(models.Model):
-    title = models.CharField(max_length=120)
-    category = models.CharField(max_length=120)
-    description = models.TextField(max_length=500)
-    price = models.FloatField()
-
-    def __str__(self):
-        return f'{self.title} costs {self.price}'
-
-
 class Category(models.Model):
     name = models.CharField(max_length=120)
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    category = models.ForeignKey(Category, null=True,  max_length=120, on_delete=models.SET_NULL)
+    description = models.TextField(max_length=500)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f'{self.title} costs {self.price}'
 
