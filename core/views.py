db307a1cbd58c2ddaa36f70cbceefe6e1339c6dd
barto <bartosz.marmolowski@gmail.com>
2020-10-04 11:38:32 +0200

    Add views to core

--- core/views.py
+++ core/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 
 
+class ProductGoToCartView(DetailView):
+    pass
 
+
+class ProductListView(ListView):
+    pass
+
+
+class ProductDetailView(DetailView):
+    pass
+
+
+class ProductCreateView(CreateView):
+    pass
+
+
+class ProductDeleteView(DeleteView):
+    pass
