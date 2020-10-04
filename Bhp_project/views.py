f5668ebef63658abf61fd81f1c72975593868254
barto <bartosz.marmolowski@gmail.com>
2020-10-04 09:36:54 +0200

    Add model Product

--- Bhp_project/views.py
+++ Bhp_project/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
 
+
 class IndexView(ListView):
     title = 'Welcome to Django Movies!'
     template_name = 'index.html'
--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    category = models.CharField(max_length=120)
+    description = models.TextField(max_length=500)
+    creation_date = models.DateField()
+    price = models.FloatField()
