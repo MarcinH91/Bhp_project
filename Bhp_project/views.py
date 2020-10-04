951aade2b162599dc7777dc4fef1d3b916b3f047
barto <bartosz.marmolowski@gmail.com>
2020-10-04 13:17:12 +0200

    Add templates to settings.py

--- Bhp_project/views.py
+++ Bhp_project/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import ListView
 
+from core.views import ProductListView
 
-class IndexView(ListView):
+
+class IndexView(ProductListView):
     title = 'Welcome to Django Movies!'
     template_name = 'index.html'
