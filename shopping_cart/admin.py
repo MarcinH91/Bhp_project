abb54a2708050b65914995bf721a1b9a2f298ab4
Bartek <bartosz.marmolowski@gmail.com>
2020-10-08 20:08:26 +0200

    Add OrderProduct model

--- shopping_cart/admin.py
+++ shopping_cart/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from shopping_cart.models import Order
+from shopping_cart.models import Order, OrderProduct
 
 admin.site.register(Order)
+admin.site.register(OrderProduct)
--- shopping_cart/models.py
+++ shopping_cart/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 
+class OrderProduct(models.Model):
+    pass
+
+
 class Order(models.Model):
     pass
