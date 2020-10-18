a43187738d07eb6796085bd44582b580ae29b6ef
Marcin Harasim <marcin.sopot@gmail.com>
2020-10-18 09:53:45 +0200

    frontend completed

--- sendemail/urls.py
+++ sendemail/urls.py
@@ -6,4 +6,5 @@ app_name = 'sendemail'
 
 urlpatterns = [
     path('contact', views.ContactFormView.as_view(), name='contact'),
+    path('contact/success', views.SuccessFormView.as_view(), name='success'),
 ]
